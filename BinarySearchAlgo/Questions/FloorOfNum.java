a54925a5a41fcd640104a0c196c87199719b336e
Lakshay Kumar <imlakshaykumar@gmail.com>
2023-01-31 00:13:21 +0530

    Questions of Binary Search Algorithm

--- BinarySearchAlgo/Questions/FloorOfNum.java
+++ BinarySearchAlgo/Questions/FloorOfNum.java
@@ -1,22 +1,21 @@
 package BinarySearchAlgo.Questions;
 
 /*
- * Ceiling: Find the smallest no. in th array which is greater or equal to the target element
+ * Find the greatest no. in th array which is smaller or equal to the target element
  * 
  * eg: if target is 15, 
- * then find the number which is equal to or greater than 15 which is 16 (greater) or 15 (equal)
+ * then find the number which is equal to or greater than 15 which is 15 (equal) or 14 (smaller)
  */
-public class CeilingFan {
+public class FloorOfNum {
     public static void main(String[] args) {
-        int[] arr = { -18, -12, -4, 0, 2, 3, 4, 16, 18, 22, 45, 89 };
+        int[] arr = { -18, -12, -4, 0, 2, 3, 4, 14, 16, 18, 22, 45, 89 };
         int target = 15;
-        int ans = ceiling(arr, target);
+        int ans = floorNum(arr, target);
 
-        System.out.println(ans); // 16
+        System.out.println(ans);
     }
 
-    // return the element
-    static int ceiling(int[] arr, int target) {
+    static int floorNum(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
 
@@ -31,6 +30,6 @@ public class CeilingFan {
                 return arr[mid];
             }
         }
-        return arr[start];
+        return arr[end];
     }
 }
